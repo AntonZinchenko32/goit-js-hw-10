6795a9987d9e226dd29f7522116a2a338616e154
AntonZinchenko32 <zinchenko309@gmail.com>
2023-02-14 07:30:45 +0200

    big update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,12 +3,10 @@ export function fetchCountries(countryName) {
     // Рядок з параметрами запиту на сервер
     const params = "name,capital,population,flags,languages";
 
-
-
   return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=${params}`).then(
     (response) => {
       if (!response.ok) {
-        throw new Error(response.status);
+          throw new Error(response.status);
       }
       return response.json();
     }
--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 var debounce = require('lodash.debounce');
 
 const searchInput = document.querySelector('#search-box');
 const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
 
 const DEBOUNCE_DELAY = 300;
 
 
 // Додаємо прослуховування інпуту
-searchInput.addEventListener("input", debounce(searchCountries, DEBOUNCE_DELAY));
+searchInput.addEventListener("input", debounce(handleInput, DEBOUNCE_DELAY));
 
     
-function searchCountries(event) {
+function handleInput(event) {
     
-const countryName = event.target.value.trim()
-
-  fetchCountries(countryName)
+    const countryName = event.target.value.trim()
+    
+  // Очищаем список и информацию о стране при очисте инпута
+  if (countryName === "") { countryList.innerHTML = ""; countryInfo.innerHTML = ""; }
+  else {
+    fetchCountries(countryName)
     .then((countries) => renderCountries(countries))
-    .catch((error) => console.log(error));
+    .catch((error) => { console.log(error); Notify.failure('Oops, there is no country with that name');});
+  }
 };
 
 function renderCountries(countries) {
-    
-if (countries.length > 10) console.log("Too many matches found. Please enter a more specific name.");
+    let markup;
 
+console.log(countries)
+
+if (countries.length > 10) Notify.info('Too many matches found. Please enter a more specific name.');
+
+    
 else if (countries.length > 1) {
-    const markup = countries
+    markup = countries
         .map(({ name, flags }) => {
-            return `<li><img src=${flags.svg} alt="Сountry flag" width="40"><h2>${name.official}</h2></li>`;
+            return `<li class="list-item"><img class="country-flag" src=${flags.svg} alt="Сountry flag"><b class="country-name">${name.common}</b></li>`;
     })
         .join("");
-    console.log(markup)
     countryList.innerHTML = markup;
+}
+    
+else {
+    markup = countries
+        .map(({ name, flags, capital, population, languages }) => {
+            return `
+          <div class="title-box"><img class="country-flag" src=${flags.svg} alt="Сountry flag"><h2 class="country-title">${name.official}</h2></div>
+          <p><b>Capital</b>: ${capital}</p>
+          <p><b>Population</b>: ${population}</p>
+          <p><b>Languages</b>: ${Object.values(languages).join(", ")}</p>
+        `;
+        });
+  countryInfo.innerHTML = markup;
 }
 }
